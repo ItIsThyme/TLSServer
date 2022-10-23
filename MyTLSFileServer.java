98a36c8c27175d2807a50674e7557d6c21f5c5df
ItIsThyme <tim@harrington.co.nz>
2022-10-23 13:01:48 +1300

    Part 1-3: Initialised keystores, SSLSocketFactory initiailised

---
+++ MyTLSFileServer.java
@@ -0,0 +1,65 @@
+import java.security.KeyStore;
+import java.security.cert.X509Certificate;
+import java.naming.Idap.*;
+import javax.net.*;
+import java.io.*;
+
+class MyTLSFileServer{
+
+
+
+    public static void main(String[] args){
+
+    }
+
+
+
+    
+    private static ServerSocketFactory getSSF(){
+	/*
+	 * Get an SSL Context that speaks some version
+	 * of TLS, a KeyManager that can hold certs in * X.509 format, and a JavaKeyStore (JKS)
+	 * instance
+	 */
+	SSLContext ctx =
+	    SSLContext.getInstance("TLS");
+	KeyManagerFactory kmf =
+	    KeyManagerFactory.getInstance("SunX509");
+	KeyStore ks =
+	    KeyStore.getInstance("JKS");
+
+	//Use the java Console class to securely retrieve the password for the keystore and load it
+	try{
+	    Console cons;
+	    char[] passwd;
+	    if ((cons = System.console()) != null &&
+		(passwd = cons.readPassword("[%s]", "Password:")) != null) {
+
+		//Load keystore
+		try{
+		    ks.load(new FileInputStream("server.jks"),
+			    passphrase);
+		}catch(IOException e){
+		    System.err.printline("could not read keystore: " + e);
+		    return null;
+		}
+		kmf.init(ks, passphrase);
+		//Clear password
+		java.util.Arrays.fill(passwd, ' ');
+	    }
+	}catch(Exception e){
+	    //The console somehow failed
+	    System.err.println("could not get password: " + e);
+	    return null;
+	}
+	//here we should have an initialised keyfactory  and zeroed the password array
+
+	//initialise the ssl context using the keys from the keyfactory
+	ctx.init(kmf.getKeyManagers(), null, null);
+
+	//get the ServerSocket and return it
+	SSLServerSocketFactory ssf =
+	    ctx.getServerSocketFactory();
+	return ssf;	
+    }
+}
